048df71f0da16de39c4fea12d2ce6e64a528be45
jeffslutzky <jeffslutzky@gmail.com>
2017-01-14 22:47:05 -0500

    Fixed

--- tweetbot.rb
+++ tweetbot.rb
@@ -38,17 +38,16 @@ class TrumpRegressBar
   end
 
   def tweet(percent)
-    "The Trump presidency is #{percent.round(1).to_s}% over!"
+    "The Trump presidency is #{percent.round(1).to_s}% over....."
   end
 
   def check
-    puts percent
-    puts (percent * 10) % 1
-    
-    # if (percent * 10) % 1 == 0
+    puts "Currently at #{percent}%."
+    if (percent * 10) % 5 == 0
       # i.e. if it's a multiple of 0.5 percent - 0.5, 1.0, 1.5...
       client.update(tweet(percent))
-    # end
+      puts "Tweeted '#{tweet(percent)}'"
+    end
   end
 
 end
