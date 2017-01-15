142df89fd3cbf0795ff61cfcb95c54910ba990f0
jeffslutzky <jeffslutzky@gmail.com>
2017-01-14 22:36:25 -0500

    test

--- tweetbot.rb
+++ tweetbot.rb
@@ -44,6 +44,7 @@ class TrumpRegressBar
   def check
     puts percent
     puts (percent * 10) % 1
+    
     # if (percent * 10) % 1 == 0
       # i.e. if it's a multiple of 0.5 percent - 0.5, 1.0, 1.5...
       client.update(tweet(percent))
